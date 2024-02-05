375c520d14b69e8ae4b821b9c5a56a5d72f5c3d0
zhouming <minerchow@126.com>
2024-02-05 21:05:34 +0800

    first commit

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,51 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+interface IData{
+  locationid : string,
+  Applicant : string,
+  FacilityType : string,
+  cnn : string,
+  LocationDescription : string,
+  Address : string,
+  blocklot : string,
+  block : string,
+  lot : string,
+  permit : string,
+  Status : string,
+  FoodItems : string,
+  X : string,
+  Y : string,
+  Latitude : string,
+  Longitude : string,
+  Schedule : string,
+  dayshours : string,
+  NOISent : string,
+  Approved : string,
+  Received : string,
+  PriorPermit : string,
+  ExpirationDate : string,
+  Location : string
+}
+type Data = {
+  result: IData[];
+};
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const fs = require('fs');
+  const csv = require('csv-parser');
+  const table:Array<IData> = [];
+  fs.createReadStream('./public/Mobile_Food_Facility_Permit.csv')
+  .pipe(csv())
+  .on('data', (data:any) => table.push(data))
+  .on('end', () => {
+    const datas = table.filter(item => item.locationid === req.query.name);
+    // console.log(req)
+    console.log(datas)
+    res.status(200).json({ result : req.query.name ? datas : table});
+  });
+  
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { Table , Select } from 'antd';
+import { uniqBy  } from 'lodash';
+import styles from "@/styles/Home.module.css";
+const { Column } = Table;
+interface IData{
+  locationid : string,
+  Applicant : string,
+  FacilityType : string,
+  cnn : string,
+  LocationDescription : string,
+  Address : string,
+  blocklot : string,
+  block : string,
+  lot : string,
+  permit : string,
+  Status : string,
+  FoodItems : string,
+  X : string,
+  Y : string,
+  Latitude : string,
+  Longitude : string,
+  Schedule : string,
+  dayshours : string,
+  NOISent : string,
+  Approved : string,
+  Received : string,
+  PriorPermit : string,
+  ExpirationDate : string,
+  Location : string
+}
+
+
+
+export default function Home() {
+   const [tableData,setTableData] = useState<IData[]>([]);
+   const [options,setOptions] = useState([])
+   const getData = async(options:{locationId:string}) => {
+      const res =  await axios.get(`/api/hello?name=${options.locationId}`);
+      let table :any= []
+      if(res.status === 200 && res.data && res.data.result){
+          table = uniqBy(res.data.result,'Applicant')
+          setTableData(table)
+          const option : any = table.map((item:any)=>{
+            return {
+              value:item.locationid,
+              label:item.Applicant
+            }
+          })
+          setOptions(option)
+      }
+  }
+  const onChange = (value: string) => {
+    console.log(`selected ${value}`);
+    getData({locationId:value || ''})
+  };
+  
+  const onSearch = (value: string) => {
+    console.log('search:', value || '');
+  };
+  const filterOption = (input: string, option?: { label: string; value: string }) =>
+  (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+
+  useEffect(()=>{
+    getData({locationId:''});
+  },[])
+  return (
+    <div>
+        <div className={styles.search}>search: <Select showSearch 
+                placeholder="Select a Applicant" 
+                onChange={onChange}
+                onSearch={onSearch}
+                filterOption={filterOption} 
+                options={options}
+                allowClear
+                /></div>
+        
+        <Table dataSource={tableData} scroll={{ x: 1500 }}>
+          <Column title="locationid" dataIndex="locationid" key="locationid"  />
+          <Column title="Applicant" dataIndex="Applicant" key="Applicant" className={styles['table-column']}/>
+          <Column title="FacilityType" dataIndex="FacilityType" key="FacilityType"  />
+          <Column title="cnn" dataIndex="cnn" key="cnn"  />
+          <Column title="LocationDescription" dataIndex="LocationDescription" key="LocationDescription" className={styles['table-column']} />
+          <Column title="Address" dataIndex="Address" key="Address" className={styles['table-column']}/>
+          <Column title="blocklot" dataIndex="blocklot" key="blocklot" />
+          <Column title="block" dataIndex="block" key="block" />
+          <Column title="lot" dataIndex="lot" key="lot" />
+          <Column title="permit" dataIndex="permit" key="permit" />
+          <Column title="Status" dataIndex="Status" key="Status" />
+          <Column title="FoodItems" dataIndex="FoodItems" key="FoodItems" className={styles['table-column']} />
+          <Column title="X" dataIndex="X" key="X" />
+          <Column title="Y" dataIndex="Y" key="Y" />
+          <Column title="Latitude" dataIndex="Latitude" key="Latitude" />
+          <Column title="Longitude" dataIndex="Longitude" key="Longitude" />
+          <Column title="Schedule" dataIndex="Schedule" key="Schedule" className={styles['table-column']}/>
+          <Column title="dayshours" dataIndex="dayshours" key="dayshours" />
+          <Column title="NOISent" dataIndex="NOISent" key="NOISent" />
+          <Column title="Approved" dataIndex="Approved" key="Approved" className={styles['table-column']}/>
+          <Column title="Received" dataIndex="Received" key="Received" />
+          <Column title="PriorPermit" dataIndex="PriorPermit" key="PriorPermit" />
+          <Column title="ExpirationDate" dataIndex="ExpirationDate" key="ExpirationDate" className={styles['table-column']} />
+          <Column title="Location" dataIndex="Location" key="Location" className={styles['table-column']}></Column>
+        </Table>
+    </div>
+  );
+}
